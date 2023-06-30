3069d9739ab4597bf2b0c21825167e8f2268aabe
David <forpersonalonly191@gmail.com>
2023-06-30 20:22:15 +0800

    JS DOM

--- newJSDOM/11_class/script.js
+++ newJSDOM/11_class/script.js
@@ -1,1 +1,20 @@
 //class <name_of_class> {}
+
+
+class Car{
+    color;
+    noOfWheel = 4
+    #serialNumber = "0000000";
+    #milege = 0;;
+
+
+    constructor(color, serialNumber, noOfWheel){
+        this.color = color;
+        this.#serialNumber = serialNumber
+        this.noOfWheel = noOfWheel && noOfWheel > 1 ? noOfWheel : this.noOfWheel
+    }
+}
+
+const Audi = new Car('red', 'UGGSSJA08BHS') //new Car
+
+console.log('After Object')
